d2508d731c1d163bab4a59983b18d0a14e3f4954
Alonso <alonso.rbass@gmail.com>
2024-07-30 22:18:15 +0200

    feat: creating first files

---
+++ js/game.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const canvasWidth = 1024
+const canvasHeight = 576
+
+canvas.width = canvasWidth
+canvas.height = canvasHeight
+
+
+
