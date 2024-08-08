dff731bd2853c5323ca7b09873fbf32858fda190
Alonso <alonso.rbass@gmail.com>
2024-08-08 23:30:44 +0200

    update: add control over the frame

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,34 +11,36 @@ const canvasHeight = 576;
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
+const desiredFPS = 120
+const frameTime = 1000 / desiredFPS
+
+
+let prevTime = performance.now()
+
 // Variável para armazenar o tempo do quadro anterior
-let prevTime = 0;
+let lag = 0;
 
 // Função que inicia a animação
 animate();
 function animate() {
-    // Solicita ao navegador que chame esta função antes do próximo repaint
+    const currentTime = performance.now()
+    const elapsed = currentTime - prevTime
+    prevTime = currentTime
+    lag += elapsed
+
+    handleControls()
+
+    while(lag >= frameTime){
+        // Preenche o fundo do canvas com a cor preta
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        background.update()
+        // Desenha o sprite `player` no canvas
+        player.update();
+
+        lag -= frameTime
+    }
+
     window.requestAnimationFrame(animate);
-
-    // Preenche o fundo do canvas com a cor preta
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-
-    background.update()
-    // Desenha o sprite `player` no canvas
-    player.update();
-    
-    // player2.update();
-    handleControls();
-    // Obtém o tempo atual em milissegundos
-    let currentTime = performance.now();
-    // Calcula o tempo decorrido desde o último quadro em segundos
-    let delta = (currentTime - prevTime) / 1000;
-    // Calcula o número de quadros por segundo (FPS)
-    let fps = 1 / delta;
-
-    // Atualiza o tempo do quadro anterior para o tempo atual
-    prevTime = currentTime;
-
-    // console.log(`FPS: ${fps}`);
 }
